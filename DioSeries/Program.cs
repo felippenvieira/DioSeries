f8b6d3661d2be8fa7743f79e424700f903c6064a
Felippe <felippe_nvieira@hotmail.com>
2022-02-22 20:04:58 -0300

    updated program.cs to include methods

--- DioSeries/Program.cs
+++ DioSeries/Program.cs
@@ -2,7 +2,7 @@
 
 SerieRepository repositories = new SerieRepository();
 
-static void Main(string[] args)
+void Main(string[] args)
 {
     string userOption = GetUserOption();
 
@@ -19,10 +19,10 @@ static void Main(string[] args)
             case "3":
                 UpdateSerie();
                 break;
-            case "4";
+            case "4":
                 DeleteSerie();
                 break;
-            case "5";
+            case "5":
                 ShowSerie();
                 break;
             case "C":
@@ -39,7 +39,100 @@ static void Main(string[] args)
     Console.WriteLine();
 }
 
+void ShowList()
+{
+    Console.WriteLine("Listar séries");
 
+    var list = repositories.SeriesList();
+
+    if (list.Count == 0)
+    {
+        Console.WriteLine("Nenhuma série cadastrada.");
+        return;
+    }
+
+    foreach (var serie in list)
+    {
+        Console.WriteLine("#ID {0}: - {1}", serie.returnId, serie.returnTitle);
+    }
+}
+
+void InsertSerie()
+{
+    Console.WriteLine("Inserir nova série");
+
+    foreach (int i in Enum.GetValues(typeof(Genre)))
+    {
+        Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genre), i));
+    }
+    Console.Write("Digite o gênero entre as opções acima: ");
+    int genreEntry = int.Parse(Console.ReadLine());
+
+    Console.Write("Digite o Título da Série: ");
+    string titleEntry = Console.ReadLine();
+
+    Console.Write("Digite o Ano de Início da Série: ");
+    int yearEntry = int.Parse(Console.ReadLine());
+
+    Console.Write("Digite a Descrição da Série: ");
+    string descriptionEntry = Console.ReadLine();
+
+    Serie newSerie = new Serie(id: repositories.NextId(),
+                                genre: (Genre)genreEntry,
+                                title: titleEntry,
+                                year: yearEntry,
+                                description: descriptionEntry);
+
+    repositories.Insert(newSerie);
+}
+
+void UpdateSerie()
+{
+    Console.Write("Digite o id da série: ");
+    int serieIndex = int.Parse(Console.ReadLine());
+
+    foreach (int i in Enum.GetValues(typeof(Genre)))
+    {
+        Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genre), i));
+    }
+    Console.Write("Digite o gênero entre as opções acima: ");
+    int genreEntry = int.Parse(Console.ReadLine());
+
+    Console.Write("Digite o Título da Série: ");
+    string titleEntry = Console.ReadLine();
+
+    Console.Write("Digite o Ano de Início da Série: ");
+    int yearEntry = int.Parse(Console.ReadLine());
+
+    Console.Write("Digite a Descrição da Série: ");
+    string descriptionEntry = Console.ReadLine();
+
+    Serie updateSerie = new Serie(id: serieIndex,
+                                genre: (Genre)genreEntry,
+                                title: titleEntry,
+                                year: yearEntry,
+                                description: descriptionEntry);
+
+    repositories.Update(serieIndex, updateSerie);
+}
+
+void ShowSerie()
+{
+    Console.Write("Digite o id da série: ");
+    int serieIndex = int.Parse(Console.ReadLine());
+
+    var serie = repositories.ReturnById(serieIndex);
+
+    Console.WriteLine(serie);
+}
+
+void DeleteSerie()
+{
+    Console.Write("Digite o id da série: ");
+    int serieIndex = int.Parse(Console.ReadLine());
+
+    repositories.Delete(serieIndex);
+}
 
 static string GetUserOption()
 {
